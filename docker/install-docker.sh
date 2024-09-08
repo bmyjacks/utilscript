6b6b5e0f3cc99c2b6ad5e7bb0f62c1eb568a4e36
Mingyi BAO <dev@bmyjacks.io>
2024-09-08 21:22:48 +0800

    Change apt-get to apt in docker/installing

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
 # Remove old versions
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt remove $pkg; done
 
+# Install dependencies
+sudo apt update
 sudo apt install ca-certificates curl -y
+
+# Add Docker's official GPG key
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add the repository to apt sources(not in the format of DEB822):
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-# Update the package index
-sudo apt update
 
 # Install Docker
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
