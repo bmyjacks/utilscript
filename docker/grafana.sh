0ec0b624e95cc0bf31c58ddde8ea9ec910d85553
Mingyi BAO <dev@bmyjacks.io>
2024-09-10 16:22:16 +0800

    Remove grafana & influxdb scripts
    Grafana and influxdb are included in TIG docker compose

--- docker/grafana.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo docker run -d \
-    -p 3000:3000 \
-    --name=grafana \
-    --mount src=grafana-storage,dst=/var/lib/grafana \
-    grafana/grafana-enterprise
--- docker/influxdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo docker run \
-    -d \
-    --name influxdb2 \
-    -p 8086:8086 \
-    --mount type=volume,source=influxdb2-data,target=/var/lib/influxdb2 \
-    --mount type=volume,source=influxdb2-config,target=/etc/influxdb2 \
-    influxdb:2
