55a61b0f804aaa045724637f1ddc88766005c036
Mingyi BAO <github@bmyjacks.io>
2024-09-21 16:14:50 +0800

    Add post-install for docker

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -14,11 +14,18 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to apt sources(not in the format of DEB822):
 echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # Install Docker
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add user to docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Enable and start the Docker service
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
